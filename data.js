015eaf92b4ace663ee4d5ab99d90ef08f4fd9ad3
ofirday <ofird79@gmail.com>
2021-10-30 08:29:52 +0300

    initial commit

---
+++ data.js
@@ -0,0 +1,43 @@
+const records = [
+    {
+        id: 1,
+        x: new Date(2015, 10, 24, 07, 40, 52),
+        y: 901344,
+        linkUrl: "http://placehold.it/32x32"
+    },
+    {
+        id: 2,
+        x: new Date(2014, 09, 17, 03, 38, 09),
+        y: 610755,
+        linkUrl: "http://placehold.it/32x32",
+        conDesc: "ABC"
+    },
+    {
+        id: 3,
+        x: new Date(2019, 01, 22, 05, 26, 10),
+        y: 574272,
+        linkUrl: "http://placehold.it/32x32",
+        conDesc: "DFE"
+    },
+    {
+        id: 4,
+        x: new Date(2014, 06, 22, 07, 42, 04),
+        y: 196551,
+        linkUrl: "http://placehold.it/32x32",
+        conDesc: "GHI"
+    },
+    {
+        id: 5,
+        x: new Date(2019, 04, 28, 09, 36, 00),
+        y: 1294558,
+        linkUrl: "http://placehold.it/32x32",
+        conDesc: "JKL"
+    },
+    {
+        id: 6,
+        x: new Date(2014, 05, 08, 02, 41, 42),
+        y: 1331981,
+        linkUrl: "http://placehold.it/32x32",
+        conDesc: "MNO"
+    }
+]
